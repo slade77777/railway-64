2f5204a30b9597c4675e3fc0ca706df3510e16bf
cuongdang <cuongdx@bfast.dev>
2023-06-28 21:13:57 +0700

    higher order + reduce + currying func

--- index.ts
+++ index.ts
@@ -32,68 +32,115 @@
 // test();
 
 // kiem tra 1 so chia het cho 30
+//
+// function checkIfSeparate2(val: number) {
+//   return new Promise((resolve, reject) => {
+//     console.log('kiem tra so 2',val);
+//     setTimeout(() => {
+//       if (val % 2 === 0) {
+//         resolve('chia het cho 2')
+//       } else {
+//         reject('khong chia het cho 2')
+//       }
+//     }, 2000)
+//   })
+// }
+//
+// function checkIfSeparate3(val: number) {
+//   return new Promise((resolve, reject) => {
+//     console.log('kiem tra so 3',val);
+//     setTimeout(() => {
+//       if (val % 3 === 0) {
+//         resolve('chia het cho 3')
+//       } else {
+//         reject('khong chia het cho 3')
+//       }
+//     }, 3000)
+//   })
+// }
+//
+// function checkIfSeparate5(val: number) {
+//   return new Promise((resolve, reject) => {
+//     console.log('kiem tra so 5',val);
+//     setTimeout(() => {
+//       if (val % 5 === 0) {
+//         resolve('chia het cho 5')
+//       } else {
+//         reject('khong chia het cho 5')
+//       }
+//     }, 5000)
+//   })
+// }
+//
+// function checkIfSeparate30(val: number) {
+//   checkIfSeparate2(val).then((res) => {
+//     checkIfSeparate3(val).then((res) => {
+//       checkIfSeparate5(val).then((res) => {
+//         console.log('chia het cho 30')
+//       }).catch(error => console.log(error))
+//     }).catch(error => console.log(error))
+//   }).catch(error => console.log(error))
+// }
+//
+// async function checkIfSeparate30Async(val: number) {
+//   try {
+//     await checkIfSeparate2(val);
+//     await checkIfSeparate3(val);
+//     await checkIfSeparate5(val);
+//     console.log('chia het cho 30')
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
+//
+// checkIfSeparate30Async(5);
+// checkIfSeparate30Async(2);
+// checkIfSeparate30Async(12);
+// checkIfSeparate30Async(30);
 
-function checkIfSeparate2(val: number) {
-  return new Promise((resolve, reject) => {
-    console.log('kiem tra so 2',val);
-    setTimeout(() => {
-      if (val % 2 === 0) {
-        resolve('chia het cho 2')
-      } else {
-        reject('khong chia het cho 2')
-      }
-    }, 2000)
-  })
+//higher order function
+function higherFunction(functionA) {
+  // logic dung chung
+  console.log('da goi vao function: ');
+
+  // logic rieng
+  return functionA;
 }
 
-function checkIfSeparate3(val: number) {
-  return new Promise((resolve, reject) => {
-    console.log('kiem tra so 3',val);
-    setTimeout(() => {
-      if (val % 3 === 0) {
-        resolve('chia het cho 3')
-      } else {
-        reject('khong chia het cho 3')
-      }
-    }, 3000)
-  })
-}
+const multiple = higherFunction(function (a, b) {
+  console.log(a * b)
+})
 
-function checkIfSeparate5(val: number) {
-  return new Promise((resolve, reject) => {
-    console.log('kiem tra so 5',val);
-    setTimeout(() => {
-      if (val % 5 === 0) {
-        resolve('chia het cho 5')
-      } else {
-        reject('khong chia het cho 5')
-      }
-    }, 5000)
-  })
-}
+const sum = higherFunction(function (a, b) {
+  console.log(a + b)
+})
 
-function checkIfSeparate30(val: number) {
-  checkIfSeparate2(val).then((res) => {
-    checkIfSeparate3(val).then((res) => {
-      checkIfSeparate5(val).then((res) => {
-        console.log('chia het cho 30')
-      }).catch(error => console.log(error))
-    }).catch(error => console.log(error))
-  }).catch(error => console.log(error))
-}
+// multiple(10, 4)
+// sum(10, 4)
 
-async function checkIfSeparate30Async(val: number) {
-  try {
-    await checkIfSeparate2(val);
-    await checkIfSeparate3(val);
-    await checkIfSeparate5(val);
-    console.log('chia het cho 30')
-  } catch (e) {
-    console.log(e)
+
+// reduce function
+const listNumber = [4 , 3, 3, 5, 1, 5];
+
+const totalSum = listNumber.reduce((total, item) => total + item, 0)
+const multipleSum = listNumber.reduce((total, item) => total * item, 1)
+
+console.log(totalSum);
+console.log(multipleSum);
+
+
+//curring function
+
+function multipleNumber(a: number) {
+  return function (b) {
+    console.log(a * b)
   }
 }
 
-checkIfSeparate30Async(5);
-checkIfSeparate30Async(2);
-checkIfSeparate30Async(12);
-checkIfSeparate30Async(30);
+const multiple2 = multipleNumber(2);
+const multiple3 = multipleNumber(3);
+const multiple4 = multipleNumber(4);
+
+console.log(multiple2(5))
+console.log(multiple3(5))
+console.log(multiple4(5))
